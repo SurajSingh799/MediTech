320aa1a93eba60e7ae143feb9b1b49f984b44a47
Suraj Bhan Singh <surajbhansingh757@gmail.com>
2025-07-04 23:24:29 +0530

    Updated environment.prod.ts with live backend URL

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [HttpClientModule , RouterModule ,FormsModule ] , // ✅ include here
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'front';
+  constructor(private http: HttpClient) {
+    // You can now use HttpClient inside this component
+  }
 }
---
+++ src/app/environment.ts
@@ -0,0 +1,5 @@
+
+export const environment = {
+  production: true,
+  apiUrl: 'https://meditech-1-6jou.onrender.com/api'
+};
